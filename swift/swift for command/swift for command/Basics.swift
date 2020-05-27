f19c0db0a61011f28ce3b295b88e8c3630599760
robert-l-520 <luoyao@brytcc.com>
2020-05-27 20:16:20 +0800

    swift init

---
+++ swift/swift for command/swift for command/Basics.swift
@@ -0,0 +1,15 @@
+//
+//  Basics.swift
+//  swift for command
+//
+//  Created by luoyao on 2018/12/22.
+//  Copyright © 2018 luoyao. All rights reserved.
+//
+
+import Foundation
+
+class testBasics{
+    
+}
+
+
