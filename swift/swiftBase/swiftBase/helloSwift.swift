aab11908bf4433584762df148e4dd00bc215b399
Ken Luo <ken.luo.5227@gmail.com>
2023-10-12 14:31:41 +0800

    update

---
+++ swift/swiftBase/swiftBase/helloSwift.swift
@@ -0,0 +1,13 @@
+//
+//  helloSwift.swift
+//  swiftBase
+//
+//  Created by ken-work on 10/12/23.
+//
+
+import Foundation
+
+var x = 10;
+var y = 11;
+
+
---
+++ swift/swiftBase/swiftBase/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  swiftBase
+//
+//  Created by ken-work on 10/12/23.
+//
+
+import Foundation
+
+print("Hello, World!")
+
